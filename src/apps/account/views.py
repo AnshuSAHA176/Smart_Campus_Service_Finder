4d1df9977c4991e5124a4ea2418d6d944fbb6b37
Anshu Saha <anshusahaa62@gmail.com>
2026-09-15 13:16:32 +0530

    register added

--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework_simplejwt.tokens import RefreshToken,AccessToken
+from .models import User
+from rest_framework.permissions import AllowAny
+from .serializer import RegisterSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes=[AllowAny]
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+
+class 
