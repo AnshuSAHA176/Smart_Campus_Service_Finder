40338ee5e3d05b27cbf6442ade495f181e9c0b01
Anshu Saha <anshusahaa62@gmail.com>
2026-09-22 12:45:46 +0530

    logout view added

--- src/apps/account/urls.py
+++ src/apps/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import LoginView,RegisterView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView,TokenBlacklistView
 
 urlpatterns = [
     path('login/',LoginView.as_view(),name= 'login'),
     path('register/',RegisterView.as_view(),name= 'register'),
     path('refresh/',TokenRefreshView.as_view(),name='for geting new access token'),
-    path('logout/',)
+    path('logout/',TokenBlacklistView.as_view(),name="for invalidate the jwt token")
 ]
